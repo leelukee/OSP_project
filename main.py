ba57b4c4d0cd17447b657786eb5080c252fc269a
leelukee <tcleverly01@gmail.com>
2019-06-16 15:51:44 +0900

    output integration

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ from bs4 import BeautifulSoup
 import re
 import sys
 
+import time
+
 app = Flask(__name__)
+
+craw_list = []
+url_list = []
 
 def web_crawling(url):
 	
+	start = time.time()
+
 	dic = {}
 	#input html에서 URL을 받아옴
         #url = request.form['URL']
@@ -24,13 +31,22 @@ def web_crawling(url):
         words = re.sub('<.+?>', '', words, 0, re.I|re.S)
         words = words.lower().strip().split()
 
-        #전체 단어수 세기
-        count = 0
+        #전체 단어수 세기(중복 포함) 
+        count = len(words)
+	#단어 수 세기(중복 미포함)
+	d = {} 
+	for word in words:
+		if word in d:
+			d[word] += 1
+		else:
+			d[word] = 1	
+
+	count2 = len(d)
 
         #처리시간 체크
-	time = 0
+	time_check = time.time() - start
 
-	dic = { "words" : words , "count" : count , "time" : time }
+	dic = { "words" : words , "count" : count , "count2" : count2 , "time" : time_check }
 	return dic
 
 
@@ -44,20 +60,21 @@ def crawl_url():
 	if request.method == 'POST':
 		url = request.form['URL']
 		#input html에서 URL을 받아옴 
-		
-
-		#crw = web_crawling(url)
+		url_list.append(url)	
 	
+		crw = web_crawling(url)
+		craw_list.append(crw)
 		
 		#crawl 결과를 output.html로 리턴 
-		return render_template('output.html', url = url)
-
+		#return render_template('output.html', url = url, crw = crw)
+		return render_template('txtout.html', url_list = url_list, craw_list = craw_list)
+	
 @app.route('/txt_url',methods = ['POST','GET'])
 def txt_url():
 	if request.method == 'POST':
 		#text file이름을 받아옴
 		txt=request.form['textname']
-		url_list= []
+		#url_list= []
 
 		#test_txt폴더에 있는 txt파일들중 이름이 같은 파일을 찾아서 파일을 연다
 		f = open("./test_txt/"+txt+".txt",'r')
@@ -67,10 +84,13 @@ def txt_url():
 			if not line: break
 
 			url_list.append(line)
-				
+
+			crw = web_crawling(line)						
+			craw_list.append(crw)
+		
 		f.close()
 		#url리스트들을 txtout.html로 넘김
-		return render_template('txtout.html', url_list = url_list)
+		return render_template('txtout.html', url_list = url_list, craw_list = craw_list)
 
 
 
